f05346e228ace6a7b3967b3758de055b9a5332bd
cupcakefromchina <32159528+cupcakefromchina@users.noreply.github.com>
2020-02-13 16:20:29 +0800

    Update ex10_31.cpp

--- ch10/ex10_31.cpp
+++ ch10/ex10_31.cpp
@@ -16,9 +16,7 @@
 int main()
 {
     std::istream_iterator<int> in_iter(std::cin), eof;
-    std::vector<int> vec;
-    while (in_iter != eof)
-        vec.push_back(*in_iter++);
+    std::vector<int> vec(in_iter,eof);
     std::sort(vec.begin(), vec.end());
     std::unique_copy(vec.cbegin(), vec.cend(), std::ostream_iterator<int>(std::cout, " "));
 }
